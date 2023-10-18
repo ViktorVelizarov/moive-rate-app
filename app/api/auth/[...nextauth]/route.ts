40e0aa2388f654c2a5f0cf7c6893837d4bfdca93
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-18 22:00:58 +0200

    add NextAuth linked with GitHub

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth"
+import type { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import DiscordProvider from "next-auth/providers/discord";
+import FacebookProvider from "next-auth/providers/facebook";
+
+  export const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!
+    }),
+    DiscordProvider({
+        clientId: process.env.DISCORD_CLIENT_ID!,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET!
+    }),
+    FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID!,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
+    })
+  ],
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
