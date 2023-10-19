e3ece7edc4fadb58d3baa833aa34f963fb86496c
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-19 08:57:27 +0200

    add google and discord auth login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import type { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import DiscordProvider from "next-auth/providers/discord";
 import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
 
   export const authOptions: NextAuthOptions = {
   providers: [
@@ -17,7 +18,12 @@ import FacebookProvider from "next-auth/providers/facebook";
     FacebookProvider({
         clientId: process.env.FACEBOOK_CLIENT_ID!,
         clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
     })
+    
   ],
 }
 
