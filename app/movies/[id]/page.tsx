da642f7d06799cb9e9058189d0b8cb7a1706cfa6
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-21 11:14:18 +0200

    start designing the movie page

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,4 +1,7 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { Button } from '@/components/ui/button';
 import { prisma } from '@/lib/prisma';
+import { BarChart4, Star } from 'lucide-react';
 import { Metadata } from 'next';
 
 interface Props {
@@ -23,7 +26,40 @@ export default async function fetchMovieByID({ params }: Props) {
         const result = await response.json();
         console.log(result)
         return (
-            <h1>Hi</h1>
+            <MaxWidthWrapper>
+            <div className='flex flex-row mt-20 justify-between'>
+                <div className='flex flex-col'>
+                    <h1 className='text-5xl font-semibold'>{result.title}</h1>
+                    <div className='flex flex-row gap-4'>
+                        <p>2023</p>
+                        <p>PG-13</p>
+                        <p>2h 15m</p>
+                    </div>
+                </div>
+
+                <div className='flex flex-col'>
+                    <div className='flex flex-row gap-5'>
+                        <p>Movie Rating</p>
+                        <p>Your Rating</p>
+                        <p>Popularity</p>
+                    </div>
+                    <div className='flex flex-row gap-5'>
+                        <Button>
+                            <Star color='#facd05' fill='#facd05'/>
+                            7.2/10
+                        </Button>
+                        <Button>
+                        <Star color='#0c8ff2'/>
+                            <span className='text-blueImport'>Rate</span>
+                        </Button>
+                        <Button>
+                        <BarChart4 color='green'/>
+                            35
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            </MaxWidthWrapper>
         )
     } catch (error) {
         console.error(error);
