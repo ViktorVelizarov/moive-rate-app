019122511387fd9eb68f963413f275173510417f
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-25 23:25:06 +0200

    add the buttons for Playing Now movies too

--- components/PlayingMovies.tsx
+++ components/PlayingMovies.tsx
@@ -5,6 +5,7 @@ import UpcomingMovCard from './MovieCard';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import MovieCard from './MovieCard';
 import { ChevronRight } from 'lucide-react';
+import PlayingMoviesDiv from './PlayingMoviesDiv';
 
 export default async function playingMovies() {
     const playingMovies = await fetchPlaying();
@@ -12,29 +13,6 @@ export default async function playingMovies() {
 
     const isDataEmpty = !Array.isArray(playingMovies.results) || playingMovies.results.length < 1 || !playingMovies.results;
   return (  
-    <MaxWidthWrapper>
-    <main className='bg-black mt-20'>
-    <div className='flex flex-row mb-5'>
-    <h1 className='text-white font-semibold text-xl'><span className='text-yellowImport'>|</span> Playing Now:</h1>
-    <ChevronRight color='white'/>
-    </div>
-    {!isDataEmpty ? (
-          <section>
-            <div className='flex flex-row gap-3'>
-              {playingMovies.results?.slice(1, 7).map((upcomingMovie: {id: string, vote_average: number, title: string,poster_path: string, backdrop_path: string; } ) => (
-                
-                <MovieCard title={upcomingMovie.title} poster={upcomingMovie.poster_path} id={upcomingMovie.id} rating={upcomingMovie.vote_average}/>
-              ))}
-            </div>
-          </section>
-        ) : (
-          <div>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-          </div>
-        )}
-
-   
-    </main>
-    </MaxWidthWrapper>
+    <PlayingMoviesDiv isDataEmpty={isDataEmpty} playingMovies={playingMovies}/>
   ) 
 }
---
+++ components/PlayingMoviesDiv.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { ChevronLeftSquare, ChevronRight, ChevronRightSquare } from 'lucide-react'
+import MovieCard from './MovieCard'
+
+export default function PlayingMoviesDiv({isDataEmpty, playingMovies} : any) {
+
+    const [startingIndex, SetStartingIndex] = React.useState(0)
+    const [endingIndex, SetEndingIndex] = React.useState(6)
+
+    function goBack(){
+        if(startingIndex != 0)
+        {
+        SetStartingIndex(startingIndex => startingIndex-1)
+        SetEndingIndex(endingIndex => endingIndex-1)
+        }
+    }
+
+    function goFront(){
+        if(endingIndex < 20)
+        {
+            SetStartingIndex(startingIndex => startingIndex+1)
+            SetEndingIndex(endingIndex => endingIndex+1)
+        }
+    }
+
+  return (
+    <MaxWidthWrapper>
+    <main className='bg-black mt-20'>
+    <div className='flex flex-row justify-between'>
+      <div className='flex flex-row mb-5'>
+      <h1 className='text-white font-semibold text-xl'><span className='text-yellowImport'>|</span> Playing Now:</h1>
+    
+      <ChevronRight color='white'/>
+      </div>
+      <div className='flex flex-row'>
+      <ChevronLeftSquare onClick={goBack} className='w-10 h-10 bg-yellowImport rounded-md mr-2'/>
+      <ChevronRightSquare onClick={goFront} className='w-10 h-10 bg-yellowImport rounded-md'/>
+      </div>
+    </div>
+    {!isDataEmpty ? (
+          <section>
+            <div className='flex flex-row gap-3'>
+              {playingMovies.results?.slice(startingIndex, endingIndex).map((upcomingMovie: {id: string, vote_average: number, title: string,poster_path: string, backdrop_path: string; } ) => (
+                
+                <MovieCard title={upcomingMovie.title} poster={upcomingMovie.poster_path} id={upcomingMovie.id} rating={upcomingMovie.vote_average}/>
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          </div>
+        )}
+
+   
+    </main>
+    </MaxWidthWrapper>
+  )
+}
